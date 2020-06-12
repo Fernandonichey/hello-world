925e62200c8016bbcd5dc2353142179510016871
NicheyFernando <51688618+Fernandonichey@users.noreply.github.com>
2020-06-12 10:03:52 +0800

    Update FileNameExtrator.py
    update the csv and xls output

--- FileNameExtrator.py
+++ FileNameExtrator.py
@@ -1,35 +1,44 @@
 import os
-import json
-from PIL import Image
 import csv
 import codecs
 import xlwt
 
+__all__=["FileNameExtract_To_txt","FileNameExtract_To_csv","FileNameExtract_To_xls"]
 __filelist = []
+__file_extension_selection = []
 
 
 def _file_extension(file_name):
     return os.path.splitext(file_name)[1]
 
 
-def _getAllfiles(Image_Path, file_extension_check=True):
+def _getAllfiles(Input_Path):
+    if not os.path.exists(Input_Path):
+        print("Your input path" + Input_Path + "is not exist")
+        return
     '''file_extension_check means to check the file is image or not'''
-    image_extension = ['.bmp', '.jpg', '.jpeg', '.png', '.tiff']
-    filenames = os.listdir(Image_Path)
+    filenames = os.listdir(Input_Path)
     for file in filenames:
-        fileabspath = os.path.join(Image_Path, file)
+        fileabspath = os.path.join(Input_Path, file)
         if os.path.isdir(fileabspath):
             _getAllfiles(fileabspath)
         else:
-            if _file_extension(file) in image_extension:
+            if len(__file_extension_selection):
+                if _file_extension(file) in __file_extension_selection:
+                    __filelist.append(fileabspath)
+            else:
                 __filelist.append(fileabspath)
     return __filelist
 
 
-def FileNameExtract_To_txt(Ori_Path='', Saved_path_file_name=''):  # filename为写入CSV文件的路径，data为要写入数据列表.
-    if Ori_Path == '':
+def FileNameExtract_To_txt(Input_Path='', Saved_path_file_name='',
+                           file_extension_selection=[]):  # filename为写入CSV文件的路径，data为要写入数据列表.
+    for item in file_extension_selection:
+        __file_extension_selection.append(item)
+    if not os.path.exists(Input_Path):
+        print("Your input path" + Input_Path + "is not exist")
         return
-    data = _getAllfiles(Ori_Path)
+    data = _getAllfiles(Input_Path)
     file = open(Saved_path_file_name, 'w')
     for i in range(len(data)):
         s = str(data[i]).replace('[', '').replace(']', '')  # 去除[],这两行按数据不同，可以选择
@@ -37,25 +46,34 @@ def FileNameExtract_To_txt(Ori_Path='', Saved_path_file_name=''):  # filename为
         file.write(s)
     file.close()
     __filelist.clear()
+    __file_extension_selection.clear()
     print(Saved_path_file_name + " has been saved successfully")
 
 
-def FileNameExtract_To_csv(Ori_Path='', Saved_path_file_name=''):  # file_name为写入CSV文件的路径，datas为要写入数据列表
-    if Ori_Path == '':
+def FileNameExtract_To_csv(Input_Path='', Saved_path_file_name='',
+                           file_extension_selection=[]):  # file_name为写入CSV文件的路径，datas为要写入数据列表
+    for item in file_extension_selection:
+        __file_extension_selection.append(item)
+    if not os.path.exists(Input_Path):
+        print("Your input path" + Input_Path + "is not exist")
         return
-    datas = _getAllfiles(Ori_Path)
+    datas = _getAllfiles(Input_Path)
     file_csv = codecs.open(Saved_path_file_name, 'w+', 'utf-8')  # 追加
-    writer = csv.writer(file_csv, delimiter=' ', quotechar=' ', quoting=csv.QUOTE_NONE)
+    writer = csv.writer(file_csv,delimiter=' ',quotechar=' ', quoting=csv.QUOTE_MINIMAL)
     for data in datas:
-        writer.writerow(data)
+        writer.writerow(str(data))
     __filelist.clear()
+    __file_extension_selection.clear()
     print(Saved_path_file_name + " has been saved successfully")
 
 
-def FileNameExtract_To_xls(Ori_Path='', Saved_path_file_name=''):
-    if Ori_Path == '':
+def FileNameExtract_To_xls(Input_Path='', Saved_path_file_name='', file_extension_selection=[]):
+    for item in file_extension_selection:
+        __file_extension_selection.append(item)
+    if not os.path.exists(Input_Path):
+        print("Your input path" + Input_Path + "is not exist")
         return
-    datas = _getAllfiles(Ori_Path)
+    datas = _getAllfiles(Input_Path)
     f = xlwt.Workbook()
     sheet1 = f.add_sheet(u'sheet1', cell_overwrite_ok=True)  # 创建sheet
 
@@ -65,15 +83,16 @@ def FileNameExtract_To_xls(Ori_Path='', Saved_path_file_name=''):
         i = i + 1
     f.save(Saved_path_file_name)  # 保存文件
     __filelist.clear()
+    __file_extension_selection.clear()
     print(Saved_path_file_name + " has been saved successfully")
 
 
 if __name__ == '__main__':
-    Path = R'C:\Users\ren5szh\Desktop\Station1Crop\\'
-    if Path == '':
-        pass
-    else:
-
-        FileNameExtract_To_txt(Saved_path_file_name=Path + 'files.txt', Ori_Path=Path)
-        FileNameExtract_To_csv(Saved_path_file_name=Path + 'files.csv', Ori_Path=Path)
-        FileNameExtract_To_xls(Saved_path_file_name=Path + 'files.xls', Ori_Path=Path)
+    Path =""  # os.getcwd()
+    if Path:
+        FileNameExtract_To_txt(Input_Path=Path, Saved_path_file_name=Path + 'files.txt',
+                               file_extension_selection=['.py','.bmp'])
+        FileNameExtract_To_csv(Input_Path=Path, Saved_path_file_name=Path + 'files.csv',
+                               file_extension_selection=['.py','.bmp'])
+        FileNameExtract_To_xls(Input_Path=Path, Saved_path_file_name=Path + 'files.xls',
+                               file_extension_selection=['.py','.bmp'])
